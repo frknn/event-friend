a0d1edeb796981f3e3cd547cee8c1054e0925cdb
frknn <frknstbs@gmail.com>
2019-08-21 11:31:04 +0300

    some refactoring for auth tables

--- EventFriendJava/src/main/resources/data.sql
+++ EventFriendJava/src/main/resources/data.sql
@@ -1,2 +1,20 @@
-insert into event (baslik,bulusma_yeri,detay,etkinlik_adresi,il,ilce,kac_kisi) values ('Halı Saha','100.Yıl Halı Sahası','Yarın akşam 8-9 saatlerinde halı sahaya 2 kişi eksiğimiz var.','100.Yıl Mah. Kemalpaşa Sokak.','Karabük','Merkez',2)
-insert into event (baslik,bulusma_yeri,detay,etkinlik_adresi,il,ilce,kac_kisi) values ('Konser','Çarşı','Konsere fazladan bir biletim var','Bağlar Mahallesi Fevzi Çakmak Caddesi, Espark','Eskişehir','Tepebaşı',1)
+drop table if exists oauth_access_token;
+create table oauth_access_token (
+  token_id VARCHAR(256),
+  token LONGVARBINARY,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication LONGVARBINARY,
+  refresh_token VARCHAR(256)
+);
+
+drop table if exists oauth_refresh_token;
+create table oauth_refresh_token (
+  token_id VARCHAR(256),
+  token LONGVARBINARY,
+  authentication LONGVARBINARY
+);
+
+insert into event (baslik,bulusma_yeri,detay,etkinlik_adresi,il,ilce,kac_kisi) values ('Halı Saha','100.Yıl Halı Sahası','Yarın akşam 8-9 saatlerinde halı sahaya 2 kişi eksiğimiz var.','100.Yıl Mah. Kemalpaşa Sokak.','Karabük','Merkez',2);
+insert into event (baslik,bulusma_yeri,detay,etkinlik_adresi,il,ilce,kac_kisi) values ('Konser','Çarşı','Konsere fazladan bir biletim var','Bağlar Mahallesi Fevzi Çakmak Caddesi, Espark','Eskişehir','Tepebaşı',1);
